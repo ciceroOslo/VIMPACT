25c0e67f9701dd3c333865ded684d78047c80b0b
Frode Rørvik <froder@cicero.oslo.no>
2024-10-27 12:34:16 +0100

    Pretty good version this yes!

--- vimpact.py
+++ vimpact.py
@@ -5,19 +5,15 @@
 import pandas as pd
 import os
 import openpyxl
+import warnings
 
-def process_files(hl_filename, dr_filename, mp_filename):
 
-# Read the mapping Excel file into a DataFrame. Skip the first row, as it contains the column names.
+def get_mapping_data(mp_filename):
+
+    # Read the mapping Excel file into a DataFrame. Skip the first row, as it contains the column names.
     try:
         mpdf = pd.read_excel(mp_filename)
-        
-        # Split the DataFrame into two: task_df with columns A, B, and C; spec4_df with columns D and E
-        task_df = mpdf.iloc[:, [0, 2]]
-        spec4_df = mpdf.iloc[:, [4, 5]].dropna(subset=[mpdf.columns[4]])
-        # Convert project numbers to string
-        spec4_df['Projects'] = spec4_df['Projects'].astype(str) 
-
+       
     except FileExistsError as e:
         print(f"\033[91mError: The file {e} is in use by another application or file not found.\033[0m")
         return None
@@ -26,8 +22,11 @@ def process_files(hl_filename, dr_filename, mp_filename):
         return None
     else:
         print(f"\033[92m1) The mapping Excel file was read successfully.\033[0m")
-     #   print(task_df)
-     #   print(spec4_df)
+        return mpdf
+
+
+def process_input_files(hl_filename, dr_filename):
+
 
 # Read the H&L file into a DataFrame. Use fixed-width format to read the file.
     try:
@@ -48,7 +47,8 @@ def process_files(hl_filename, dr_filename, mp_filename):
     
 # Read the supporting Visma Payroll report file (Excel) - Transaksjoner, detaljert.xlsx into a DataFrame.
     try:
-        drdf = pd.read_excel(dr_filename, skiprows=1)
+        warnings.filterwarnings("ignore", category=UserWarning, message="Workbook contains no default style, apply openpyxl's default")    
+        drdf = pd.read_excel(dr_filename, skiprows=1, engine='openpyxl')
     except FileExistsError as e:
         print(f"\033[91mError: The file {e} is in use by another application or file not found.\033[0m")
         return None
@@ -56,7 +56,7 @@ def process_files(hl_filename, dr_filename, mp_filename):
         print(f"\033[91mError reading the file. The file might be in use: {e}\033[0m")
         return None
     else:
-        print(f"\033[92m3) Payroll report Excel file read successfully. Please ignore the warning above.\033[0m")
+        print(f"\033[92m3) Payroll report Excel file read successfully.\033[0m")
 
 
 ###########################################################################################################
@@ -94,86 +94,140 @@ def process_files(hl_filename, dr_filename, mp_filename):
         # Merging: Add the column Text to hldf DataFrame and use a vlookup-like function to fetch drdf and join on ID=Reiseregning ID
         hldf['Text'] = hldf['ID'].map(drdf.set_index('Reiseregning ID')['Tekst']).fillna('Lønn ').astype(str)    
 
-    # The creation of debit/credit entries to compensate for the lack of handling invoiced expenses vs. non-invoiced expenses.
-    # The new transactions are based on data from the mapping Excel file - spec4_df dataframe.
-
-    # Looping though the accounting DataFrame and looking for account numbers that match the content of spec4_dfInsert two rows if "Konto" = 7701
-
-        new_rows = []
-        for index, row in hldf.iterrows():
-            if row['Konto'] == 7701:
-                # Create the first new row with Konto = 7999 and negative Beløp
-                new_row_1 = row.copy()
-                new_row_1['Konto'] = 7999
-                new_row_1['Beløp'] = -row['Beløp']
-                new_rows.append(new_row_1)
+         
+        #return hldf dataframe from the function
+        return hldf
         
-                # Create the second new row with Konto = 4999 and positive Beløp
-                new_row_2 = row.copy()
-                new_row_2['Konto'] = 4999
-                new_row_2['Beløp'] = abs(row['Beløp'])
-                new_rows.append(new_row_2)
-    
-            # Insert the new rows into hldf
-            if new_rows:
-                hldf = pd.concat([hldf, pd.DataFrame(new_rows)], ignore_index=True)
-        
-        # Building the Maconomy import file from the hldf DataFrame.
-        df_macloc = pd.DataFrame(columns=['GeneralJournal:Format','TransactionNumber', 'EntryDate', 'EntryText', 'TypeOfEntry', 'AccountNumber', 'FinanceVATCode', 'DebitBase', 'CreditBase','EntityName','JobNumber','TaskName','ActivityNumber','EmployeeNumber'])    
-        df_macloc['EntryDate'] = hldf['Dato'].dt.strftime('%d/%m/%Y')
-        df_macloc['EntryText'] = hldf.apply(lambda row: row['Text'] if isinstance(row['Text'], str) else None, axis=1)
-        df_macloc['TypeOfEntry'] = 'G'
-        df_macloc['AccountNumber'] = hldf.apply(lambda row: row['Konto'] if row['Prosjekt'] < 1 else None, axis=1)
-        df_macloc['FinanceVATCode'] = hldf.apply(lambda row: row['MVA'] if row['MVA'] > 0 else None, axis=1)
-        df_macloc['DebitBase'] = hldf.apply(lambda row: row['Beløp'] if row['Beløp'] > 0 else None, axis=1)
-        df_macloc['CreditBase'] = hldf.apply(lambda row: abs(row['Beløp']) if row['Beløp'] < 0 else None, axis=1)
-        df_macloc['EntityName'] = hldf.apply(lambda row: row['Avdeling'] if isinstance(row['Avdeling'], str) else None, axis=1)
-        df_macloc['ActivityNumber'] = hldf.apply(lambda row: row['Konto'] if row['Prosjekt'] > 1 else None, axis=1)
-        df_macloc['JobNumber'] = hldf.apply(lambda row: row['Prosjekt'] if row['Prosjekt'] > 0 else None, axis=1)
-        df_macloc['EmployeeNumber'] = hldf.apply(lambda row: row['Medarbeider'] if row['Medarbeider'] != '0' else None, axis=1)
-        df_macloc['GeneralJournal:Format'] = 'GENERALJOURNAL:CREATE'
-        df_macloc['TransactionNumber'] = '#KEEP'
-        
-       # print(df_macloc.all)
-
-        return df_macloc
-        #return hldf dataframe
 
     except Exception as e:
         print(f"\033[91mError processing the data: {e}\033[0m")
         return None
     
 
- #   except Exception as e:
- #      print(f"Error writing the data to Maconomy: {e}")
+# Function to create CICERO specific debit/crecit transaction for proper accounting practises
+
+def cicero_specific_transactions(input_df_hldf, input_df_mapping):
+
+    # The creation of debit/credit entries to compensate for the lack of handling invoiced expenses vs. non-invoiced expenses.
+    # The new transactions are based on data from the mapping Excel file - spec4_df dataframe.
+
+    # If the value of Prosjekt is greater than 30000 and is not found in Project coloumn in df_spec4, then run the following code.
+    
+
+    df_spec4 = input_df_mapping.iloc[:, [4,5]].dropna(subset=[input_df_mapping.columns[4]])
+
+      
+    new_rows = []
+
+    for index, row in input_df_hldf.iterrows():
+
+        if int(row['Prosjekt']) > 30000 and int(row['Prosjekt']) not in df_spec4['Projects'].astype(int).values:
+            # Create the first new row with Konto = 7999 and negative Beløp
+            new_row_1 = row.copy()
+            new_row_1['Konto'] = 7999
+            new_row_1['Beløp'] = -row['Beløp']
+            new_rows.append(new_row_1)
+     
+            # Create the second new row with Konto = 4999 and positive Beløp
+            new_row_2 = row.copy()
+            new_row_2['Konto'] = 4999
+            new_row_2['Beløp'] = abs(row['Beløp'])
+            new_rows.append(new_row_2)
+
+        elif int(row['Prosjekt']) in df_spec4[df_spec4['Spec4'] == 'Towards2040']['Projects'].astype(int).values:
+                # Create the first new row with Konto = 7999 and negative Beløp
+            new_row_1 = row.copy()
+            new_row_1['Konto'] = 7998
+            new_row_1['Beløp'] = -row['Beløp']
+            new_rows.append(new_row_1)
+             
+            # Create the second new row with Konto = 4999 and positive Beløp
+            new_row_2 = row.copy()
+            new_row_2['Konto'] = 4998
+            new_row_2['Beløp'] = abs(row['Beløp'])
+            new_rows.append(new_row_2)
+
+        elif int(row['Prosjekt']) in df_spec4[df_spec4['Spec4'] == 'Oppdrag']['Projects'].astype(int).values:
+            # Create the first new row with Konto = 7999 and negative Beløp
+            new_row_1 = row.copy()
+            new_row_1['Konto'] = 7997
+            new_row_1['Beløp'] = -row['Beløp']
+            new_rows.append(new_row_1)
+     
+            # Create the second new row with Konto = 4999 and positive Beløp
+            new_row_2 = row.copy()
+            new_row_2['Konto'] = 4997
+            new_row_2['Beløp'] = abs(row['Beløp'])
+            new_rows.append(new_row_2)
+   
+            # Insert the new rows into hldf
+        if new_rows:
+            df_addded_transactions = pd.concat([input_df_hldf, pd.DataFrame(new_rows)], ignore_index=True)
+        
+    return df_addded_transactions
+    # boooo
 
 
-def transform_to_maconomy(input_df_hldf):
-    # Example transformation: Add a new column 'Transformed' which is double the 'Beløp' column
-    transformed_df = input_df_hldf.copy()
-    transformed_df['Transformed'] = transformed_df['Beløp'] * 2
-    return transformed_df
+# Function to transform the DataFrame to a Maconomy import file format
+def transform_to_maconomy(input_df_hldf, input_df_mapping):
 
-# Example usage of the new function
-#if result_df is not None:
-#    transformed_df = transform_dataframe(result_df)
-#    print(transformed_df.head())
+    df_task = input_df_mapping.iloc[:, [0, 2]]
 
-# The main program code - make sure it only runs if the script is executed directly
-# *********************************************************************************        
+    # Building the Maconomy import file from the hldf DataFrame.
+    df_macloc = pd.DataFrame(columns=['GeneralJournal:Format','TransactionNumber', 'EntryDate', 'EntryText', 'TypeOfEntry', 'AccountNumber', 'FinanceVATCode', 'DebitBase', 'CreditBase','EntityName','JobNumber','TaskName','ActivityNumber','EmployeeNumber'])    
+    df_macloc['EntryDate'] = input_df_hldf['Dato'].dt.strftime('%d/%m/%Y')
+    df_macloc['EntryText'] = input_df_hldf.apply(lambda row: row['Text'] if isinstance(row['Text'], str) else None, axis=1)
+    df_macloc['TypeOfEntry'] = 'G'
+    df_macloc['AccountNumber'] = input_df_hldf.apply(lambda row: row['Konto'] if row['Prosjekt'] < 1 else None, axis=1)
+    df_macloc['FinanceVATCode'] = input_df_hldf.apply(lambda row: row['MVA'] if row['MVA'] > 0 else None, axis=1)
+    df_macloc['DebitBase'] = input_df_hldf.apply(lambda row: row['Beløp'] if row['Beløp'] > 0 else None, axis=1)
+    df_macloc['CreditBase'] = input_df_hldf.apply(lambda row: abs(row['Beløp']) if row['Beløp'] < 0 else None, axis=1)
+    df_macloc['EntityName'] = input_df_hldf.apply(lambda row: row['Avdeling'] if isinstance(row['Avdeling'], str) else None, axis=1)
+    df_macloc['ActivityNumber'] = input_df_hldf.apply(lambda row: row['Konto'] if row['Prosjekt'] > 1 else None, axis=1)
+    df_macloc['JobNumber'] = input_df_hldf.apply(lambda row: row['Prosjekt'] if row['Prosjekt'] > 0 else None, axis=1)
+    df_macloc['EmployeeNumber'] = input_df_hldf.apply(lambda row: row['Medarbeider'] if row['Medarbeider'] != '0' else None, axis=1)
+   # added with copilot
+    df_macloc['TaskName'] = df_macloc['ActivityNumber'].map(df_task.set_index('Account')['Task'])
+    df_macloc['GeneralJournal:Format'] = 'GENERALJOURNAL:CREATE'
+    df_macloc['TransactionNumber'] = '#KEEP'
+
+    return df_macloc
+
+
+
+
+# ***********************************************************************************
+# The main program code - make sure it only runs if the script is executed directly *
+# ***********************************************************************************        
 
 if __name__ == "__main__":
-    # Example usage
+    # Define the file paths and input files
     downloads_dir = os.path.join(os.path.expanduser("~"), "Downloads")
     hl_filename = os.path.join(downloads_dir, "HLTrans_971274190_202411.HLT")
     dr_filename = os.path.join(downloads_dir, "Transaksjoner, detaljert.xlsx")
     mp_filename = os.path.join("mapping.xlsx")
 
-    result_df = process_files(hl_filename, dr_filename, mp_filename)
+    # Processing and preparing the accounting data
+    accounting_df = process_input_files(hl_filename, dr_filename)
 
+    # Getting the mapping data from the Excel file
+    mapping_df = get_mapping_data(mp_filename)
 
-    if result_df is not None:
+    # Adding CICERO specific debit/credit transactions to the accounting data
+    cicero_accounting_df = cicero_specific_transactions(accounting_df, mapping_df)
+
+    # Transforming the accounting data to Maconomy format.
+    maconomy_df = transform_to_maconomy(cicero_accounting_df, mapping_df)
+
+    # Writing the Maconomy DataFrame to an Excel file
+    if maconomy_df is not None:
         output_filename = os.path.join(os.path.dirname(hl_filename), "out.xlsx")
-        result_df.to_excel(output_filename, index=False)
-                       
-        print(f"DataFrame written to {output_filename} successfully.")
+
+        try:
+            maconomy_df.to_excel(output_filename, index=False)
+        except Exception as e:
+            print(f"\033[91mError writing the Maconomy import file: {e}\033[0m")
+        else:                      
+            print(f"\033[95mDataFrame written to {output_filename} successfully.\033[0m")
+
+### Fine
