473fd0e167eaadc4ec8a11f69657cf54983ffc1f
Frode Rørvik <froder@cicero.oslo.no>
2024-10-29 22:40:25 +0100

    Release-kandidat til økonomi for kontroll

--- vimpact.py
+++ vimpact.py
@@ -6,13 +6,10 @@ import pandas as pd
 import os
 import warnings
 
-
+# Read the mapping Excel file into a DataFrame. Skip the first row, as it contains the column names.
 def get_mapping_data(mp_filename):
-
-    # Read the mapping Excel file into a DataFrame. Skip the first row, as it contains the column names.
     try:
-        mpdf = pd.read_excel(mp_filename)
-       
+        mpdf = pd.read_excel(mp_filename)   
     except FileExistsError as e:
         print(f"\033[91mError: The file {e} is in use by another application or file not found.\033[0m")
         return None
@@ -24,12 +21,15 @@ def get_mapping_data(mp_filename):
         return mpdf
 
 # Read the H&L file into a DataFrame. Use fixed-width format to read the file.
-def process_input_files(hl_filename, dr_filename):
+def process_input_files(hl_filename, dr_filename, mp_dataframe):
+
+    # Mapping - Task lookup from Account.
+    mp = mp_dataframe.iloc[:, [0, 2]]
+
     try:
-        # Define the column specifications for the fixed-width file
+        # Define the column specifications for the fixed-width Visma Payroll accounting file
         hlcolspecs = [(0, 12), (12, 14), (14, 26), (26, 38), (38, 50), (50, 62), (62, 74), (74, 86), (86, 98), (98, 118), (118, 121), (121, 129), (129, 139), (139, 149), (149, 160) ] 
         hlcolumn_names = ['Konto', 'MVA', 'Avdeling', 'Prosjekt', 'Medarbeider', 'R4', 'R5', 'R6', 'R7', 'ID', 'Filler', 'Dato', 'Ant', 'Sats', 'Beløp']  
-
         # Read the fixed-width file into a DataFrame
         hldf = pd.read_fwf(hl_filename, colspecs=hlcolspecs, names=hlcolumn_names)
     except FileExistsError as e:
@@ -40,8 +40,8 @@ def process_input_files(hl_filename, dr_filename):
         return None
     else:    
         print(f"\033[92m2) The HL Payroll accounting file read successfully.\033[0m")
-    
-# Read the supporting Visma Payroll report file (Excel) - Transaksjoner, detaljert.xlsx into a DataFrame.
+
+    # Read the supporting Visma Payroll report file (Excel) - Transaksjoner, detaljert.xlsx into a DataFrame.
     try:
         warnings.filterwarnings("ignore", category=UserWarning, message="Workbook contains no default style, apply openpyxl's default")    
         drdf = pd.read_excel(dr_filename, skiprows=1, engine='openpyxl')
@@ -54,13 +54,15 @@ def process_input_files(hl_filename, dr_filename):
     else:
         print(f"\033[92m3) Payroll report Excel file read successfully.\033[0m")
 
-
-###########################################################################################################
-# Data processing and transformation
-###########################################################################################################
+    #########################################################################################################
+    # Data processing and transformation
+    #########################################################################################################
     try:
         # Removing the columns that are not needed in the Payroll accounting dataframe
         hldf.drop(columns=['R4', 'R5', 'R6', 'R7', 'Filler', 'Ant', 'Sats'], inplace=True)
+
+        # Insert column 'Oppgave' between 'Prosjekt' and 'Medarbeider'
+        hldf.insert(hldf.columns.get_loc('Medarbeider'), 'Oppgave', None)
 
         # Converting to proper datatypes - hldf DataFrame
         hldf['Dato'] = pd.to_datetime(hldf['Dato'], format='%d%m%Y', errors='coerce')
@@ -68,104 +70,140 @@ def process_input_files(hl_filename, dr_filename):
         hldf['Konto'] = pd.to_numeric(hldf['Konto'], errors='coerce')
         hldf['ID'] = pd.to_numeric(hldf['ID'], errors='coerce')
 
-        # If Konto < 5900 and Prosjekt > 0, then Prosjekt = 0
         # Removing unwanted Project and Department dimensions. Just to correct sub-optimal configuration of Visma Payroll.
-        # might be removed in the future.
-        hldf.loc[(hldf['Konto'] < 5900) & (hldf['Prosjekt'] > 0), 'Prosjekt'] = 0
+        # This might be removed in the future.
+        hldf.loc[(hldf['Konto'] < 5320) | (hldf['Konto'] > 5329) & (hldf['Konto'] < 5800) & (hldf['Prosjekt'] > 0), 'Prosjekt'] = 0
         hldf.loc[(hldf['Medarbeider'] == "0") & (hldf['Prosjekt'] == 0), 'Avdeling'] = 0
 
-        # Use pivot function to aggregate Beløp if Konto and Avdeling and Project are the same
+        # Populate 'Oppgave' with Task from mp. Mapping should be done on Konto=Account.
+        mp.columns = ['Account', 'Task']
+
+        # If Prosjekt > 10000 then map Oppgave from Konto using the mapping DataFrame else Oppgave = 0        
+        hldf.loc[hldf['Prosjekt'] > 10000, 'Oppgave'] = hldf['Konto'].map(mp.set_index('Account')['Task'])
+        hldf.loc[hldf['Prosjekt'] < 10000, 'Oppgave'] = 0
+        
+        # Use pivot function to aggregate Beløp if Konto & Avdeling & Project are the same
         # might be removed in the future.
-        hldf = hldf.pivot_table(index=['Konto', 'Avdeling', 'Prosjekt', 'Medarbeider','MVA', 'ID', 'Dato'], values='Beløp', aggfunc='sum').reset_index()
+        hldf = hldf.pivot_table(index=['Konto', 'Avdeling', 'Prosjekt', 'Medarbeider','Oppgave', 'MVA', 'ID', 'Dato'], values='Beløp', aggfunc='sum').reset_index()
 
-        # Drop the columns Lønnsperiode, Ansattnummer og Lønnsart from drdf DataFrame in the Payroll report dataframe
-        drdf.drop(columns=['Lønnsperiode', 'Ansattnummer', 'Lønnsart', 'Beløp'], inplace=True)
+        # Drop the columns Lønnsperiode, Ansattnummer og Lønnsart from drdf DataFrame (Payroll Excel report)
+        drdf.drop(columns=['Lønnsperiode', 'Ansattnummer', 'Lønnsart', 'Beløp', 'MVA-kode'], inplace=True)
 
-        # Use pivot function to aggregate drdf DataFrame if Tekst and Reiseregning ID are the same
+        # Use pivot function to aggregate drdf if rows are duplicated (Tekst & Reiseregning ID)
         drdf = drdf.pivot_table(index=['Tekst', 'Reiseregning ID']).reset_index()
 
-        # Converting the 'Reiseregning ID' column to a integer object to make merging/mapping more robust.
+        # Converting the 'Reiseregning ID' to an integer object to make merging/mapping more robust.
         drdf['Reiseregning ID'] = pd.to_numeric(drdf['Reiseregning ID'], errors='coerce')
         
         # Merging: Add the column Text to hldf DataFrame and use a vlookup-like function to fetch drdf and join on ID=Reiseregning ID     
-        hldf['Text'] = hldf.apply(lambda row: f"{drdf.set_index('Reiseregning ID')['Tekst'].get(row['ID'], f'Lønn ({row['Dato'].strftime('%Y-%m-%d')})')}" if 'Lønn' in drdf.set_index('Reiseregning ID')['Tekst'].get(row['ID'], f'Lønn ({row['Dato'].strftime('%Y-%m-%d')})') else f"{drdf.set_index('Reiseregning ID')['Tekst'].get(row['ID'], f'Lønn ({row['Dato'].strftime('%Y-%m-%d')})')} ({row['ID']})", axis=1)
-
-        #return hldf dataframe from the function
-        return hldf
-        
+        hldf['Text'] = hldf.apply(lambda row: f"{drdf.set_index('Reiseregning ID')['Tekst'].get(row['ID'], 
+        f'Lønn ({row['Dato'].strftime('%Y-%m-%d')})')}" 
+        if 'Lønn' in drdf.set_index('Reiseregning ID')['Tekst'].get(row['ID'], 
+        f'Lønn ({row['Dato'].strftime('%Y-%m-%d')})') 
+        else f"{drdf.set_index('Reiseregning ID')['Tekst'].get(row['ID'], 
+        f'Lønn ({row['Dato'].strftime('%Y-%m-%d')})')} ({row['ID']})", axis=1)
 
     except Exception as e:
         print(f"\033[91mError processing the data: {e}\033[0m")
         return None
+    finally:
+        #return hldf dataframe from the function
+        
+        print(f"\033[94mRescuing Visma from the clutches of product manager misadventures...\033[0m")
+        return hldf
     
 
 # Function to create CICERO specific debit/crecit transaction for proper accounting practises
+def cicero_specific_transactions(input_df_hldf, input_df_mapping):   
+    print(f"\033[96mProcessing CICERO specific transactions...\033[0m")
 
-def cicero_specific_transactions(input_df_hldf, input_df_mapping):
-
-    # The creation of debit/credit entries to compensate for the lack of handling invoiced expenses vs. non-invoiced expenses.
-    # The new transactions are based on data from the mapping Excel file - spec4_df dataframe.
-
-    # If the value of Prosjekt is greater than 30000 and is not found in Project coloumn in df_spec4, then run the following code.
+    # The creation of debit/credit entries to reflect invoiced expenses vs. non-invoiced expenses in the general ledger.
+    # The new debit/credit entries are applied on invoicable project (Project>30000) 
+    # There are special debit/credit entries for Towards2040 projects (listed in the mapping file)
+    # The VAT handling is also considered in the new debit/credit entries.
     
-
-    df_spec4 = input_df_mapping.iloc[:, [4,5]].dropna(subset=[input_df_mapping.columns[4]])
-
-      
+    # Extracting the Towards2040 projects (df_towards).
+    df_towards = input_df_mapping.iloc[:, [4]].dropna(subset=[input_df_mapping.columns[4]])
+    # Extracting the projects with VAT handeling (df_VAT).
+    df_VAT = input_df_mapping.iloc[:, [6]].dropna(subset=[input_df_mapping.columns[6]])
+       
     new_rows = []
 
+    # Ivoicable projects (>30000) 
+    # 5000-5998: Debit 4753, Kredit 5999
+    # 6000-6998: Debit 4753, Kredit 6XXX
+    # 7000-7998: Debit 4753, Kredit 7999
+
+    # Towards (df_towards)
+    # 5000-5998: Debit 7713, Kredit 5999
+    # 6000-6998: Debit 7713, Kredit 6XXX
+    # 7000-7998: Debit 7713, Kredit 7999
+    
+    # Loop through the DataFrame and create the new debit/credit entries
     for index, row in input_df_hldf.iterrows():
 
-        if int(row['Prosjekt']) > 30000 and int(row['Prosjekt']) not in df_spec4['Projects'].astype(int).values:
-            # Create the first new row with Konto = 7999 and negative Beløp
-            new_row_1 = row.copy()
-            new_row_1['Konto'] = 7999
-            new_row_1['Beløp'] = -row['Beløp']
-            new_rows.append(new_row_1)
+        # Deleting VAT codes from non VAT projects
+        if int(row['Prosjekt']) not in df_VAT['Project_VAT'].astype(int).values:   
+            input_df_hldf.at[index, 'MVA'] = 0 
+            row['MVA'] = 0
+
+        if int(row['Prosjekt']) > 30000 and int(row['Prosjekt']) not in df_towards['Towards'].astype(int).values:
+            # Ordenary invoiable project - credit trans.
+            if int(row['Konto']) in range(5000, 5999):
+                new_row_1 = row.copy()
+                new_row_1['Konto'] = 5999
+                new_row_1['Beløp'] = -row['Beløp']
+                new_rows.append(new_row_1)
+            elif int(row['Konto']) in range(6000, 6999):
+                new_row_1 = row.copy()
+                new_row_1['Konto'] = ['Konto']
+                new_row_1['Beløp'] = -row['Beløp']
+                new_rows.append(new_row_1)
+            elif int(row['Konto']) in range(7000, 7999):
+                new_row_1 = row.copy()
+                new_row_1['Konto'] = 7999
+                new_row_1['Beløp'] = -row['Beløp']
+                new_rows.append(new_row_1)    
      
-            # Create the second new row with Konto = 4999 and positive Beløp
+            # Debit trans on account 4753
             new_row_2 = row.copy()
-            new_row_2['Konto'] = 4999
+            new_row_2['Konto'] = 4753
             new_row_2['Beløp'] = abs(row['Beløp'])
             new_rows.append(new_row_2)
 
-        elif int(row['Prosjekt']) in df_spec4[df_spec4['Spec4'] == 'Towards2040']['Projects'].astype(int).values:
-                # Create the first new row with Konto = 7999 and negative Beløp
-            new_row_1 = row.copy()
-            new_row_1['Konto'] = 7998
-            new_row_1['Beløp'] = -row['Beløp']
-            new_rows.append(new_row_1)
+        elif int(row['Prosjekt']) in df_towards['Towards'].astype(int).values:
+            # Towards2040 projects (df_towards) - credit trans.
+            if int(row['Konto']) in range(5000, 5999):
+                new_row_1 = row.copy()
+                new_row_1['Konto'] = 5999
+                new_row_1['Beløp'] = -row['Beløp']
+                new_rows.append(new_row_1)
+            elif int(row['Konto']) in range(6000, 6999):
+                new_row_1 = row.copy()
+                new_row_1['Konto'] = ['Konto']
+                new_row_1['Beløp'] = -row['Beløp']
+                new_rows.append(new_row_1)
+            elif int(row['Konto']) in range(7000, 7999):
+                new_row_1 = row.copy()
+                new_row_1['Konto'] = 7999
+                new_row_1['Beløp'] = -row['Beløp']
+                new_rows.append(new_row_1)    
              
-            # Create the second new row with Konto = 4999 and positive Beløp
+            # Debit trans on account 7713
             new_row_2 = row.copy()
-            new_row_2['Konto'] = 4998
+            new_row_2['Konto'] = 7713
             new_row_2['Beløp'] = abs(row['Beløp'])
             new_rows.append(new_row_2)
-
-        elif int(row['Prosjekt']) in df_spec4[df_spec4['Spec4'] == 'Oppdrag']['Projects'].astype(int).values:
-            # Create the first new row with Konto = 7999 and negative Beløp
-            new_row_1 = row.copy()
-            new_row_1['Konto'] = 7997
-            new_row_1['Beløp'] = -row['Beløp']
-            new_rows.append(new_row_1)
-     
-            # Create the second new row with Konto = 4999 and positive Beløp
-            new_row_2 = row.copy()
-            new_row_2['Konto'] = 4997
-            new_row_2['Beløp'] = abs(row['Beløp'])
-            new_rows.append(new_row_2)
-   
-            # Insert the new rows into hldf
+                   
+        # Insert the new rows into hldf
         if new_rows:
             df_addded_transactions = pd.concat([input_df_hldf, pd.DataFrame(new_rows)], ignore_index=True)
         
     return df_addded_transactions
 
-
-# Function to transform the DataFrame to a Maconomy import file format
-def transform_to_maconomy(input_df_hldf, input_df_mapping):
-
-    df_task = input_df_mapping.iloc[:, [0, 2]]
+# Data transformation to Maconomy import file format.
+def transform_to_maconomy(input_df_hldf):
+    print(f"\033[93mTransforming the data to Maconomy format...\033[0m")
 
     # Building the Maconomy import file from the hldf DataFrame.
     df_macloc = pd.DataFrame(columns=['GeneralJournal:Format','TransactionNumber', 'EntryDate', 'EntryText', 'TypeOfEntry', 'AccountNumber', 'FinanceVATCode', 'DebitBase', 'CreditBase','EntityName','JobNumber','TaskName','ActivityNumber','EmployeeNumber'])    
@@ -180,12 +218,10 @@ def transform_to_maconomy(input_df_hldf, input_df_mapping):
     df_macloc['ActivityNumber'] = input_df_hldf.apply(lambda row: row['Konto'] if row['Prosjekt'] > 1 else None, axis=1)
     df_macloc['JobNumber'] = input_df_hldf.apply(lambda row: row['Prosjekt'] if row['Prosjekt'] > 0 else None, axis=1)
     df_macloc['EmployeeNumber'] = input_df_hldf.apply(lambda row: row['Medarbeider'] if row['Medarbeider'] != '0' else None, axis=1)
-    df_macloc['TaskName'] = df_macloc['ActivityNumber'].map(df_task.set_index('Account')['Task'])
+    df_macloc['TaskName'] = input_df_hldf.apply(lambda row: row['Oppgave'] if row['Oppgave'] else None, axis=1)
     df_macloc['GeneralJournal:Format'] = 'GENERALJOURNAL:CREATE'
     df_macloc['TransactionNumber'] = '#KEEP'
-
     return df_macloc
-
 
 # ***********************************************************************************
 # The main program code - make sure it only runs if the script is executed directly *
@@ -198,18 +234,18 @@ if __name__ == "__main__":
     dr_filename = os.path.join(downloads_dir, "Transaksjoner, detaljert.xlsx")
     mp_filename = os.path.join("mapping.xlsx")
 
-    # Processing and preparing the accounting data
-    accounting_df = process_input_files(hl_filename, dr_filename)
-
     # Getting the mapping data from the Excel file
     mapping_df = get_mapping_data(mp_filename)
+
+    # Processing and preparing the accounting data
+    accounting_df = process_input_files(hl_filename, dr_filename, mapping_df)
 
     # Adding CICERO specific debit/credit transactions to the accounting data
     cicero_accounting_df = cicero_specific_transactions(accounting_df, mapping_df)
 
     # Transforming the accounting data to Maconomy format.
-    # Note: If you do not want the CICERO-sepcific transactions, you can specify accounting_df instead of cicero_accounting_df.
-    maconomy_df = transform_to_maconomy(cicero_accounting_df, mapping_df)
+    # NB: If you do not want the CICERO-sepcific transactions, you can specify accounting_df instead of cicero_accounting_df.
+    maconomy_df = transform_to_maconomy(cicero_accounting_df)
 
     # Static assignment of a DataFrame with three columns
     mac_header_df = pd.DataFrame({
